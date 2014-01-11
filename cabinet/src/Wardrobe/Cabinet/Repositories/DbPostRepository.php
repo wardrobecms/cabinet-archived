746d611f7d393bebb8115dc48b4728b80b754c33
Eric Barnes <eric@ericlbarnes.com>
2014-01-11 17:04:33 -0500

    Adjust query remove facadeSearch

--- cabinet/src/Wardrobe/Cabinet/Repositories/DbPostRepository.php
+++ cabinet/src/Wardrobe/Cabinet/Repositories/DbPostRepository.php
@@ -94,7 +94,6 @@ class DbPostRepository implements PostRepositoryInterface {
 
 		return Post::with(array('tags', 'user'))
 			->select('posts.*')
-			->join('tags', 'posts.id', '=', 'tags.post_id')
 			->where(function($query) use ($search)
 			{
 				$query->orWhere('title', 'like', '%'.$search.'%')
@@ -106,43 +105,6 @@ class DbPostRepository implements PostRepositoryInterface {
 			->groupBy('id')
 			->distinct()
 			->paginate($per_page);
-	}
-
-	/**
-	 * Search from the wardrobe facet
-	 *
-	 * @param  array   $params
-	 * @return array
-	 */
-	public function facadeSearch(array $params)
-	{
-		$search = isset($params['q']) ? $params['q'] : null;
-		$tag = isset($params['tag']) ? $params['tag'] : null;
-		$limit = isset($params['limit']) ? (int) $params['limit'] : 1;
-
-		$post = Post::with(array('tags', 'user'))
-			->select('posts.*')
-			->join('tags', 'posts.id', '=', 'tags.post_id')
-			->orderBy('posts.publish_date', 'desc')
-			->where('posts.active', 1)
-			->where('posts.publish_date', '<=', new DateTime)
-			->distinct();
-
-		if ($search)
-		{
-			$post->where(function($query) use ($search)
-			{
-				$query->orWhere('title', 'like', '%'.$search.'%')
-							->orWhere('content', 'like', '%'.$search.'%');
-			});
-		}
-
-		if ($tag)
-		{
-			$post->where('tags.tag', '=', $tag);
-		}
-
-		return $post->skip(0)->take($limit)->get();
 	}
 
 	/**
--- cabinet/src/helpers.php
+++ cabinet/src/helpers.php
@@ -3,8 +3,8 @@
 /**
  * Theme Path
  *
- * Helper that allows you to easily get a theme path inside the views.
- * Example: {{ asset(theme_path('css/style.css')) }}
+ * Helper that allows you to easily get a theme path inside the view
+ * Here is an example: `{{ asset(theme_path('css/style.css')) }}`
  *
  * @param string $file - The file to load
  * @return string
@@ -17,8 +17,8 @@ function theme_path($file = null)
 /**
  * Theme View Path
  *
- * Helper that allows you to easily get a theme view path inside the views.
- * Example: @extends(theme_view('layout'))
+ * Helper that allows you to easily get a theme view path
+ * Here is an example: `@extends(theme_view('layout'))`
  *
  * @param string $file - The file to load
  * @return string
@@ -26,4 +26,23 @@ function theme_path($file = null)
 function theme_view($file = null)
 {
 	return "themes.". Config::get('wardrobe.theme') .'.'.$file;
+}
+
+/**
+ * Markdown Helper
+ *
+ * Helper that allows you to easily get a theme view path inside the views.
+ * This uses a wrapper so its easy to be overridden from the base app if
+ * prefered. Here is a quick example of how you can use it md(string)
+ *
+ * @param string $str
+ *
+ * @return string
+ */
+if ( ! function_exists('md'))
+{
+	function md($str)
+	{
+		return $str;
+	}
 }
