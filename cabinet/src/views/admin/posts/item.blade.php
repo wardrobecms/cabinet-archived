c94608fd685f684d03d04eb56294607f806ac914
Eric Barnes <eric@ericlbarnes.com>
2014-01-16 20:34:43 -0500

    Removed files

--- cabinet/src/views/admin/posts/item.blade.php
+++
@@ -1,7 +0,0 @@
-<td><a href="{{ URL::route('wardrobe.post.edit', $post->id) }}">{{ $post->title }}</a></td>
-<td>{{ $post->active }}</td>
-<td>{{ $post->publish_date }}</td>
-<td class="actions">
-	<button class="preview"><i class="icon-zoom-in"></i>Preview</button>
-	<button class="delete"><i class="icon-trash"></i> Delete</button>
-</td>
