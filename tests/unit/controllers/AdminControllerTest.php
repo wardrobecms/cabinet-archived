ef55c3356257769e065914c94d327b89b6d78379
MetalMatze <me@metalmatze.de>
2014-01-21 00:55:01 +0100

    in AdminControllerTest.php use Wardrobe\Cabinet\TestCase.php

--- tests/unit/controllers/AdminControllerTest.php
+++ tests/unit/controllers/AdminControllerTest.php
@@ -1,29 +1,20 @@
 <?php namespace Wardrobe\Cabinet\Controllers;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
-use Mockery;
-use Orchestra\Testbench\TestCase;
+use Wardrobe\Cabinet\TestCase;
 
 class AdminControllerTest extends TestCase {
 
 	public function setUp()
 	{
 		parent::setUp();
-
-		Route::get('/', array('uses' => 'Wardrobe\\Cabinet\\Controllers\\AdminController@index'));
-	}
-
-	public function tearDown()
-	{
-		Mockery::close();
 	}
 
 	public function testIndex()
 	{
 		View::shouldReceive('make')->once()->with('cabinet::admin.index')->andReturn('admin index!');
 
-		$this->action('GET', 'Wardrobe\\Cabinet\\Controllers\\AdminController@index');
+		$this->action('GET', self::$wardrobeControllers . 'AdminController@index');
 	}
 }
  
