69a1006d059b61091238c91d7318c36bb603ef49
MetalMatze <me@metalmatze.de>
2014-01-21 00:52:27 +0100

    in Wardrobe\Cabinet\TestCase use \Orchestra\Testbench\TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,31 @@
-<?php
+<?php namespace Wardrobe\Cabinet;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase {
+use Illuminate\Support\Facades\Route;
+use Mockery;
 
-	protected function setUp()
+abstract class TestCase extends \Orchestra\Testbench\TestCase {
+
+	protected static $wardrobeControllers = 'Wardrobe\\Cabinet\\Controllers\\';
+
+	public function setUp()
 	{
+		parent::setUp();
 
+		$this->routes();
 	}
 
 	protected function tearDown() {
 		Mockery::close();
+	}
+
+	private function routes()
+	{
+		Route::get('/', array('uses' => self::$wardrobeControllers.'AdminController@index', 'as' => 'wardrobe.admin.index'));
+		Route::get('logout', array('uses' => self::$wardrobeControllers.'LoginController@destroy', 'as' => 'wardrobe.admin.logout'));
+		Route::get('login', array('uses' => self::$wardrobeControllers.'LoginController@create', 'as' => 'wardrobe.admin.login'));
+		Route::post('login', array('uses' => self::$wardrobeControllers.'LoginController@store'));
+		Route::get('login/remind', array('uses' => self::$wardrobeControllers.'LoginController@remindForm', 'as' => 'wardrobe.admin.remindForm'));
+		Route::post('login/remind', array('uses' => self::$wardrobeControllers.'LoginController@remindSend'));
 	}
 
 }
