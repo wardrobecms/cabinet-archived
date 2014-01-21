f16ea64849e71e2fcd6d2eb1c93963e49f3e4267
MetalMatze <me@metalmatze.de>
2014-01-21 21:49:29 +0100

    create tests for LoginController@store() if unsuccessful

--- tests/unit/controllers/LoginControllerTest.php
+++ tests/unit/controllers/LoginControllerTest.php
@@ -36,12 +36,25 @@ class LoginControllerTest extends TestCase {
 
 		Redirect::shouldReceive('intended')->once()->with('wardrobe.admin.index')->andReturn('wardrobe.admin.index');
 
-		$this->app->instance('Wardrobe\Cabinet\Repositories\UserRepositoryInterface', $this->userRepositoryInterface);
-
 		$response = $this->action('POST', self::$wardrobeControllers . 'LoginController@store', [], $input);
 
 		$this->assertSame('wardrobe.admin.index', $response->original);
 	}
 
+	public function testStoreUnsuccessful()
+	{
+		$input = ['email' => 'me@example.com', 'password' => 'wardrobe', 'remember' => true];
+
+		$this->userRepositoryInterface
+			->shouldReceive('login')->once()->with($input['email'], $input['password'], $input['remember'])->andReturn(false);
+
+		Redirect::shouldReceive('action')->once()->with('wardrobe.admin.login')->andReturn(Mockery::self())
+				->shouldReceive('withInput')->once()->withNoArgs()->andReturn(Mockery::self())
+				->shouldReceive('with')->once()->with('login_errors', true)->andReturn('wardrobe.admin.login');
+
+		$response = $this->action('POST', self::$wardrobeControllers . 'LoginController@store', [], $input);
+
+		$this->assertSame('wardrobe.admin.login', $response->original);
+	}
 }
  
