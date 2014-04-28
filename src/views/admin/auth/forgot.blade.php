871797e81045b6582c7b6c884d817ecf9e1e624d
Eric Barnes <eric@ericlbarnes.com>
2014-04-28 14:50:26 -0400

    Fixed forgot pass view

--- src/views/admin/auth/forgot.blade.php
+++ src/views/admin/auth/forgot.blade.php
@@ -1,28 +1,28 @@
-@extends('core::admin.layout')
+@extends('cabinet::admin.layout')
 
 @section('title')
-  {{ Lang::get('core::wardrobe.forgot_password') }}
+  {{ Lang::get('cabinet::wardrobe.forgot_password') }}
 @stop
 
 @section('content')
   <div id="login-region">
-    <h1>{{ Lang::get('core::wardrobe.forgot_password') }}</h1>
+    <h1>{{ Lang::get('cabinet::wardrobe.forgot_password') }}</h1>
     @if (Session::has('error'))
       <div class="alert alert-block alert-error">
         <p>
           {{ trans(Session::get('reason')) }}
-          {{ trans(Session::get('core::reason')) }}
+          {{ trans(Session::get('cabinet::reason')) }}
         </p>
       </div>
     @elseif (Session::has('success'))
       <div class="alert alert-block alert-success">
-        <p>{{ Lang::get('core::wardrobe.forgot_sent') }}</p>
+        <p>{{ Lang::get('cabinet::wardrobe.forgot_sent') }}</p>
       </div>
     @endif
 
     <form method="post" action="/wardrobe/login/remind" class="form-horizontal">
-      <p><input type="text" id="inputEmail" name="email" placeholder="{{ Lang::get('core::wardrobe.account_email') }}"></p>
-      <button type="submit" class="btn">{{ Lang::get('core::wardrobe.forgot_send') }}</button>
+      <p><input type="text" id="inputEmail" name="email" placeholder="{{ Lang::get('cabinet::wardrobe.account_email') }}"></p>
+      <button type="submit" class="btn btn-primary">{{ Lang::get('cabinet::wardrobe.forgot_send') }}</button>
       </div>
     </form>
   </div>
