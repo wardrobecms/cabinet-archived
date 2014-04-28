1d62cd7d141c96a375900a6c1cf29a257153ab87
Eric Barnes <eric@ericlbarnes.com>
2014-04-28 15:29:23 -0400

    Add theme_view helper back

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,17 @@ function wardrobe_path($file = null)
 {
 	return asset('/packages/wardrobe/cabinet/'.$file);
 }
+
+/**
+ * Theme View Path
+ *
+ * Helper that allows you to easily get a theme view path inside the views.
+ * Example: @extends(theme_path('layout'))
+ *
+ * @param string $file - The file to load
+ * @return string
+ */
+function theme_view($file = null)
+{
+	return Config::get('wardrobe.theme').'.'.$file;
+}
